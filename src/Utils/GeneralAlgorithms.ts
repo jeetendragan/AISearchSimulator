a444fcffc4b95271704a8468343cf5a721840b91
Jeetendra Gan <jeetendra.gan@gmail.com>
2020-03-14 22:25:57 -0400

    Add topological sorting

--- src/Utils/GeneralAlgorithms.ts
+++ src/Utils/GeneralAlgorithms.ts
@@ -2,6 +2,12 @@ import { SimulatorComponent } from 'src/app/simulator/simulator.component';
 import { BipartiteChecker } from "./BipartiteChecker";
 
 export class GeneralAlgorithms{
+    /*
+    This function is used to order the graph topologically
+    */
+    static TopologicalSorting(simulatorInstance: SimulatorComponent) : any{
+        
+    }
     
     /*
     This function is used to check if a graph is bipartite or not
